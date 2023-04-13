edbd8b7fadda86d21a0439346ea84e29162b9bd5
Savanna McCauley <savannamccauley@yahoo.com>
2023-04-13 09:07:35 -0500

    project in progress

--- movies_application.js
+++ movies_application.js
@@ -1,24 +1,63 @@
 "use strict";
+// Load movies function
 
-document.getElementById("loader").style.display = "block";
-fetch("http://localhost:3000/movies")
-    .then(response => response.json())
-    .then(movies => {
-        let htmlString = "";
-        for (let movie of movies) {
-            htmlString += "<div class='card' style='width: 18rem'>";
-            htmlString += "<div class='card-body'>";
-            htmlString += " <h2 class='card-text'>" + `${movie.title}` + "</h2>";
-            htmlString += "<p class='card-text'>" + `${movie.rating}` + "</p>";
-            htmlString += "</div>";
-            htmlString += "</div>";
-        }
+function loadMovies() {
+    document.getElementById("loader").style.display = "block";
+    fetch("http://localhost:3000/movies")
+        .then(response => response.json())
+        .then(movies => {
+            let htmlString = "";
+            for (let movie of movies) {
+                htmlString += "<div class='card' style='width: 18rem'>";
+                htmlString += "<div class='card-body'>";
+                htmlString += " <h2 class='card-text' class='text'>" + `${movie.title}` + "</h2>";
+                htmlString += "<p class='card-text' class='text'>" + ` Movie Rating: ${movie.rating}` + "</p>";
+                htmlString += "<button class='card-textm delete-button' class='text' data-id='" + `${movie.id}` + "'>" + "Delete" + "</button>";
+                htmlString += "<button class='card-textm edit-button' class='text' data-id='" + `${movie.id}` + "'>" + "Edit" + "</button>";
 
-        document.getElementById("container").innerHTML = htmlString;
-        document.getElementById("loader").style.display = "none";
+                htmlString += "</div>";
+                htmlString += "</div>";
+            }
+
+            document.getElementById("container").innerHTML = htmlString;
+            document.getElementById("loader").style.display = "none";
+//deleted button-------------------------------------------------------------------------------
+            const deleteButtons = document.getElementsByClassName('delete-button');
+            for (let button of deleteButtons) {
+                button.addEventListener('click', function () {
+                    const id = this.getAttribute('data-id');
+                    deleteMovie(id);
+
+                });
+                const editButton = document.getElementsByClassName("edit-button");
+                for ( let button of deleteButtons){
+                    button.addEventListener('click',function(){
+                        const id = this.getAttribute('data-id');
+                        editMovie(id);
+                    })
+                }
+
+            }})
+        .catch(error => console.error(error));
+}
+//delete
+function deleteMovie(id) {
+    fetch(`http://localhost:3000/movies/${id}`, {
+        method: 'DELETE',
     })
-    .catch(error => console.error(error));
+        .then(resp => resp.json())
+        .then(data => {
+            console.log(data);
+            const deletedElement = document.querySelector(`.card[data-id="${id}"]`);
+            deletedElement.remove();
+        })
+        .catch(error => console.error(error));
 
+}
+
+
+// Call loadMovies on page load
+window.onload = loadMovies;
 document.getElementById('submitButton').addEventListener('click', function (e) {
     e.preventDefault();
 
@@ -35,13 +74,16 @@ document.getElementById('submitButton').addEventListener('click', function (e) {
             for (let movie of filteredMovies) {
                 htmlString += "<div class='card' style='width: 18rem'>";
                 htmlString += "<div class='card-body'>";
-                htmlString += " <h2 class='card-text'>" + `${movie.title}` + "</h2>";
-                htmlString += "<p class='card-text'>" + `${movie.rating}` + "</p>";
+                htmlString += " <h2 class='card-text' class='text'>" + `${movie.title}` + "</h2>";
+                htmlString += "<p class='card-text' class='text'>" + ` Movie Rating: ${movie.rating}` + "</p>";
+                htmlString += "<button class='card-text delete-button' class='text' data-id='" + `${movie.id}` + "'>" + "Delete" + "</button>";
+                htmlString += "<button class='card-text edit-button' class='text' data-id='" + `${movie.id}` + "'>" + "Edit" + "</button>";
                 htmlString += "</div>";
                 htmlString += "</div>";
             }
 
             document.getElementById("container").innerHTML = htmlString;
+
         })
         .catch(error => console.error(error));
 });
@@ -52,6 +94,7 @@ document.getElementById("addMovieForm").addEventListener("submit", function(even
     // Get form values
     let title = document.getElementById("title").value;
     let rating = document.getElementById("rating").value;
+
 
     // Create movie object
     let movie = {
@@ -74,34 +117,14 @@ document.getElementById("addMovieForm").addEventListener("submit", function(even
             // Clear form fields
             document.getElementById("title").value = "";
             document.getElementById("rating").value = "";
+
         })
         .catch(error => console.error(error));
 });
 
-// Load movies function
-function loadMovies() {
-    document.getElementById("loader").style.display = "block";
-    fetch("http://localhost:3000/movies")
-        .then(response => response.json())
-        .then(movies => {
-            let htmlString = "";
-            for (let movie of movies) {
-                htmlString += "<div class='card' style='width: 18rem'>";
-                htmlString += "<div class='card-body'>";
-                htmlString += " <h2 class='card-text'>" + `${movie.title}` + "</h2>";
-                htmlString += "<p class='card-text'>" + `${movie.rating}` + "</p>";
-                htmlString += "</div>"; // Fix: Add closing div tag for card-body
-                htmlString += "</div>"; // Fix: Add closing div tag for card
-            }
 
-            document.getElementById("container").innerHTML = htmlString;
-            document.getElementById("loader").style.display = "none";
-        })
-        .catch(error => console.error(error));
-}
 
-// Call loadMovies on page load
-window.onload = loadMovies;
+
 
 // Function to delete a movie
 
