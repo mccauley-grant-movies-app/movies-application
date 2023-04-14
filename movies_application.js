4a1a659d72a52baeca7364ebd4a1d426feeec49a
Savanna McCauley <savannamccauley@yahoo.com>
2023-04-14 09:28:14 -0500

    complete

--- movies_application.js
+++ movies_application.js
@@ -1,175 +1,9 @@
-// "use strict";
-// // Load movies function
-//
-// function loadMovies() {
-//     document.getElementById("loader").style.display = "block";
-//     fetch("http://localhost:3000/movies")
-//         .then(response => response.json())
-//         .then(movies => {
-//             let htmlString = "";
-//             for (let movie of movies) {
-//                 htmlString += "<div class='container'>"
-//                 htmlString += "<div class='card' style='width: 18rem' class='scrolling-wrapper-flexbox'>";
-//                 htmlString += "<div class='card-body'>";
-//                 htmlString += " <h2 class='card-text' class='text'>" + `${movie.title}` + "</h2>";
-//                 htmlString += "<p class='card-text' class='text'>" + ` Movie Rating: ${movie.rating}` + "</p>";
-//                 htmlString += "<button class='card-textm delete-button' class='text' data-id='" + `${movie.id}` + "'>" + "Delete" + "</button>";
-//                 htmlString += "<button class='card-textm edit-button' class='text' data-id='" + `${movie.id}` + "'>" + "Edit" + "</button>";
-//
-//                 htmlString += "</div>";
-//                 htmlString += "</div>";
-//                 htmlString += "</div>";
-//
-//             }
-//             htmlString = "<div class='card-container'>" + htmlString + "</div>";
-//
-//
-//             document.getElementById("container").innerHTML = htmlString;
-//             document.getElementById("loader").style.display = "none";
-// //deleted button-------------------------------------------------------------------------------
-//             const deleteButtons = document.getElementsByClassName('delete-button');
-//             for (let button of deleteButtons) {
-//                 button.addEventListener('click', function () {
-//                     const id = this.getAttribute('data-id');
-//                     deleteMovie(id);
-//
-//                 });
-//                 const editButtons = document.getElementsByClassName('edit-button');
-//                 for ( let button of editButtons){
-//                     button.addEventListener('click',function(){
-//                         const id = this.getAttribute('data-id');
-//                         editMovie(id);
-//                     })
-//                 }
-//
-//             }})
-//         .catch(error => console.error(error));
-// }
-// //delete
-// function deleteMovie(id) {
-//     fetch(`http://localhost:3000/movies/${id}`, {
-//         method: 'DELETE',
-//     })
-//         .then(resp => resp.json())
-//         .then(data => {
-//             console.log(data);
-//             const deletedElement = document.querySelector(`.card[data-id="${id}"]`);
-//             deletedElement.remove();
-//         })
-//         .catch(error => console.error(error));
-//
-// }
-//
-//
-// // Call loadMovies on page load
-// window.onload = loadMovies;
-// document.getElementById('submitButton').addEventListener('click', function (e) {
-//     e.preventDefault();
-//
-//     const titleInput = document.getElementById('searchBar').value;
-//
-//     fetch("http://localhost:3000/movies")
-//         .then(response => response.json())
-//         .then(movies => {
-//             let filteredMovies = movies.filter(function(movie) {
-//                 return movie.title.toLowerCase().includes(titleInput.toLowerCase());
-//             });
-//
-//             let htmlString = "";
-//             for (let movie of filteredMovies) {
-//                 htmlString += "<div class='card' style='width: 18rem'>";
-//                 htmlString += "<div class='card-body'>";
-//                 htmlString += " <h2 class='card-text' class='text'>" + `${movie.title}` + "</h2>";
-//                 htmlString += "<p class='card-text' class='text'>" + ` Movie Rating: ${movie.rating}` + "</p>";
-//                 htmlString += "<button class='card-text delete-button' class='text' data-id='" + `${movie.id}` + "'>" + "Delete" + "</button>";
-//                 htmlString += "<button class='card-text edit-button' class='text' data-id='" + `${movie.id}` + "'>" + "Edit" + "</button>";
-//                 htmlString += "</div>";
-//                 htmlString += "</div>";
-//             }
-//             htmlString = "<div class='card-container'>" + htmlString + "</div>";
-//
-//
-//             document.getElementById("container").innerHTML = htmlString;
-//
-//         })
-//         .catch(error => console.error(error));
-// });
-//
-// document.getElementById("addMovieForm").addEventListener("submit", function(event) {
-//     event.preventDefault();
-//
-//     // Get form values
-//     let title = document.getElementById("title").value;
-//     let rating = document.getElementById("rating").value;
-//
-//
-//     // Create movie object
-//     let movie = {
-//         title: title,
-//         rating: rating
-//     };
-//
-//     // Call API to add movie
-//     fetch("http://localhost:3000/movies", {
-//         method: "POST",
-//         headers: {
-//             "Content-Type": "application/json"
-//         },
-//         body: JSON.stringify(movie)
-//     })
-//         .then(response => response.json())
-//         .then(movies => {
-//             // Reload movie list
-//             loadMovies();
-//             // Clear form fields
-//             document.getElementById("title").value = "";
-//             document.getElementById("rating").value = "";
-//
-//         })
-//         .catch(error => console.error(error));
-// });
-//
-//
-//
-//
-//
-// // Function to edit a movie
-//
-// // Edit Movie Function
-// // Edit Movie Function
-// function editMovie(id) {
-//     // Get the movie data from the form
-//     const title = document.getElementById(`title-${id}`).value;
-//     const rating = document.getElementById(`rating-${id}`).value;
-//
-//     // Create a movie object with the updated data
-//     const updatedMovie = {
-//         title: title,
-//         rating: rating
-//     };
-//
-//     // Make a PUT request to update the movie data
-//     fetch(`http://localhost:3000/movies/${id}`, {
-//         method: 'PUT',
-//         headers: {
-//             'Content-Type': 'application/json',
-//         },
-//         body: JSON.stringify(updatedMovie),
-//     })
-//         .then(response => response.json())
-//         .then(data => {
-//             console.log(data);
-//             // Reload the movies after successful update
-//             loadMovies(updatedMovie);
-//         })
-//         .catch(error => console.error(error));
-// }
 
 "use strict";
-// Load movies function
 
+// Loads The Function
 function loadMovies() {
-    document.getElementById("loader").style.display = "block";
+    document.getElementById("loader").style.display = "block"; //loading sign
     fetch("http://localhost:3000/movies")
         .then(response => response.json())
         .then(movies => {
@@ -192,7 +26,7 @@ function loadMovies() {
 
 
             document.getElementById("container").innerHTML = htmlString;
-            document.getElementById("loader").style.display = "none";
+            document.getElementById("loader").style.display = "none"; //removes loading sign
 
             // Delete button event listener
             const deleteButtons = document.getElementsByClassName('delete-button');
@@ -215,8 +49,74 @@ function loadMovies() {
         })
         .catch(error => console.error(error));
 }
+//Search Button Function
+document.getElementById('submitButton').addEventListener('click', function (e) {
+    e.preventDefault();
 
-// Delete movie function
+    const titleInput = document.getElementById('searchBar').value;
+
+    fetch("http://localhost:3000/movies")
+        .then(response => response.json())
+        .then(movies => {
+            let filteredMovies = movies.filter(function(movie) {
+                return movie.title.toLowerCase().includes(titleInput.toLowerCase());
+            });
+
+            let htmlString = "";
+            for (let movie of filteredMovies) {
+                htmlString += "<div class='card' style='width: 18rem'>";
+                htmlString += "<div class='card-body'>";
+                htmlString += " <h2 class='card-text' class='text'>" + `${movie.title}` + "</h2>";
+                htmlString += "<p class='card-text' class='text'>" + ` Movie Rating: ${movie.rating}` + "</p>";
+                htmlString += "<button class='card-text delete-button' class='text' data-id='" + `${movie.id}` + "'>" + "Delete" + "</button>";
+                htmlString += "<button class='card-text edit-button' class='text' data-id='" + `${movie.id}` + "'>" + "Edit" + "</button>";
+                htmlString += "</div>";
+                htmlString += "</div>";
+            }
+            htmlString = "<div class='card-container'>" + htmlString + "</div>";
+
+
+            document.getElementById("container").innerHTML = htmlString;
+
+        })
+        .catch(error => console.error(error));
+});
+//Add a movie functionality
+document.getElementById("addMovieForm").addEventListener("submit", function(event) {
+    event.preventDefault();
+
+    // Get form values
+    let title = document.getElementById("title").value;
+    let rating = document.getElementById("rating").value;
+
+
+    // Create movie object
+    let movie = {
+        title: title,
+        rating: rating
+    };
+
+    // Call API to add movie
+    fetch("http://localhost:3000/movies", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(movie)
+    })
+        .then(response => response.json())
+        .then(movies => {
+            // Reload movie list
+            loadMovies();
+            // Clear form fields
+            document.getElementById("title").value = "";
+            document.getElementById("rating").value = "";
+
+        })
+        .catch(error => console.error(error));
+});
+
+// Delete
 function deleteMovie(id) {
     fetch(`http://localhost:3000/movies/${id}`, {
         method: 'DELETE',
